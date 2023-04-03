aed04a3890d1e9b8ec07fa45ec1caaa4ea4eb807
dablju <wiktordablju@gmail.com>
2023-04-03 18:45:32 +0200

    03.04 Podstawy JS

---
+++ Czesc_2_SASS_JS_Bootstrap/Dzial4_JavaScript/zadanie1/js/main.js
@@ -0,0 +1,23 @@
+// // TRESC
+// Popraw błędnie nazwane zmienne.
+
+// const fav-color = 'red'
+// conts meal = 'schabowy'
+// const 1favDRINK = 'pepsi'
+// const function = 123
+
+// Stwórz zmienne z Twoim imieniem oraz wiekiem.
+
+// Za pomocą console.log wypisz zdanie "Cześć, jestem imię i mam xlat."
+
+// // KONIEC
+
+const favColor = "red"
+const meal = "schabowy"
+const favDrink1 = "pepsi"
+const numberFunction = 123
+
+const myName = "Wiktor"
+const myAge = 18
+
+console.log(`Czesc, jestem ${myName} i mam ${myAge} lat`)
